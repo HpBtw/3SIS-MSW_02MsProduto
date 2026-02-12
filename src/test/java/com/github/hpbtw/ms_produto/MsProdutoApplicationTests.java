6b81e8ec089fff2f3d4a8241c4212012034380ba
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-11 21:37:25 -0300

    First commit

---
+++ src/test/java/com/github/hpbtw/ms_produto/MsProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hpbtw.ms_produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
